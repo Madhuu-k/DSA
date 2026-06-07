9cb872e15d1423069be2c48ad1ec0d6fb816a92f
Madhu Sudhan <2300031210@kluniversity.in>
2026-06-07 23:03:35 +0530

    Strings Problems

---
+++ Strings/Basics.h
@@ -0,0 +1,12 @@
+
+#ifndef BASICS_H
+#define BASICS_H
+#include <iostream>
+#include<vector>
+#include<string>
+#include<sstream>
+#include<unordered_map>
+#include<unordered_set>
+#include<algorithm>
+using namespace std;
+#endif //BASICS_H
---
+++ Strings/String Compression.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    string newWord = "";
+
+    int i = 0, n = s.length();
+    while (i < n) {
+        int j = i;
+        int count = 0;
+
+        while (j < n && s[i] == s[j]) {
+            count++;
+            j++;
+        }
+
+        newWord += s[i];
+        newWord += to_string(count);
+
+        i = j;
+    }
+
+    cout << newWord << endl;
+
+    return 0;
+}
